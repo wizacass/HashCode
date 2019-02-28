53947f2924d71ca08de04b5100e7189f0feffb57
Wizacass <visak.pet0@gmail.com>
2019-02-28 21:27:31 +0200

    Fixed merge errors

--- Program.cs
+++ Program.cs
@@ -13,63 +13,60 @@ namespace HashCode
         }
 
         static void Group(List<Slide> slides, List<Slide> slides2)
-        {                
+        {
             int i = 0;
             int nextSlide = -1;
-            while(slides2.Count < slides.Count)
+            while (slides2.Count < slides.Count)
             {
-                if(!slides[i].used)
+                if (!slides[i].IsUsed)
                 {
-                    
+
                     int maxPoints = -1;
-                        
-                    for(int ii = 0; ii < slides.Count; ii++)
+
+                    for (int ii = 0; ii < slides.Count; ii++)
                     {
-                        if(!slides[ii].used)
+                        if (!slides[ii].IsUsed)
                         {
-                            
-                            if(CalculatePoints(slides[i], slides[ii]) > maxPoints && i != ii)
+
+                            if (CalculatePoints(slides[i], slides[ii]) > maxPoints && i != ii)
                             {
                                 maxPoints = CalculatePoints(slides[i], slides[ii]);
                                 nextSlide = ii;
                             }
-                            
+
                         }
-                        ii++;                                
                     }
-                    
-                        
                 }
-                
-                }
-                if (nextSlide != -1)
-                {
-                    slides2.Add(slides[(int)nextSlide]);
-                    i = nextSlide;
-                }
-                
+
+            }
+            if (nextSlide != -1)
+            {
+                var current = slides[nextSlide];
+                slides2.Add(current);
+                current.Use();
+                i = nextSlide;
             }
 
-
         }
+
 
         static int CalculatePoints(Slide slide1, Slide slide2)
         {
             int common = 0;
             int unique1 = 0;
             int unique2 = 0;
-            for(int i = 0; i < slide1._tags.Count; i++)
+            for (int i = 0; i < slide1._tags.Count; i++)
             {
-                if(slide2._tags.Contains(slide1._tags[i]))
-                    common++;                
+                if (slide2._tags.Contains(slide1._tags[i]))
+                    common++;
             }
             unique1 = slide1._tags.Count - common;
             unique2 = slide2._tags.Count - common;
-            if(common <= unique1 && common <= unique2)
+            if (common <= unique1 && common <= unique2)
                 return common;
             else
             {
-                if(unique1 <= unique2)
+                if (unique1 <= unique2)
                     return unique1;
                 else return unique2;
             }
