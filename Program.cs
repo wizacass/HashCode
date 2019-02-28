545867ca777192eb515d44e8ca8f2b9f1e648105
Laurys15951 <laurys.strav@gmail.com>
2019-02-28 21:14:21 +0200

    metodas vertikaliu list'ui i slide'u list'a

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace HashCode
 {
@@ -7,6 +9,30 @@ namespace HashCode
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+        }
+
+        public List<Slide> VerticalsToSlides(List<Photo>vertical)
+        {
+            List<Slide> slides = new List<Slide>();
+            for(int i = vertical.Count/2 + vertical.Count%2; i < vertical.Count;i ++)
+            {
+                int[] overlap = new int[vertical.Count/2 + vertical.Count%2];
+                for(int j = 0; j < vertical.Count/2 + vertical.Count%2;j ++)
+                {
+                    if(vertical[j].used)
+                    {
+                        overlap[j] = 101;
+                        continue;                        
+                    }
+                    overlap[j] = vertical[i].Tags.overlap(vertical[j].Tags).Count;
+                }
+                int indexOMin = Array.IndexOf(overlap, overlap.Min());
+                vertical[i].used = true;
+                vertical[indexOMin].used = true;
+                Slide slide = new Slide(vertical[i].ID, vertical[i].Tags, vertical[indexOMin].ID, vertical[indexOMin].Tags);
+                slides.Add(slide);
+            }
+            return slides;
         }
     }
 }
